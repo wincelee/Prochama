ff89ac95ab890ca5fbb50d8c69e1c140e7e0b9f7
aguerwinceville@gmail.com <aguerwinceville@gmail.com>
2020-05-27 06:44:48 +0300

    Register fragment complete, adding firebase google-services

--- app/src/main/java/manu/apps/prochama/fragments/LoginFragment.java
+++ app/src/main/java/manu/apps/prochama/fragments/LoginFragment.java
@@ -2,8 +2,6 @@ package manu.apps.prochama.fragments;
 
 import androidx.lifecycle.ViewModelProvider;
 
-import android.content.Intent;
-import android.graphics.Paint;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -11,44 +9,28 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
 
-import android.text.Editable;
-import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import com.android.volley.AuthFailureError;
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
-import com.google.android.material.snackbar.Snackbar;
-import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.util.HashMap;
-import java.util.Map;
-
 import manu.apps.prochama.R;
-import manu.apps.prochama.classes.Config;
 
 public class LoginFragment extends Fragment implements View.OnClickListener {
 
     private LoginViewModel loginViewModel;
     private ProgressBar pbLogin;
-    private EditText etEmail;
+    private TextInputLayout tilEmail, tilPassword;
+    private EditText etEmail, etPassword;
+    private TextView tvRegister;
+
+    private Button btnLogin;
+
 
     public static LoginFragment newInstance() {
         return new LoginFragment();
@@ -70,12 +52,20 @@ public class LoginFragment extends Fragment implements View.OnClickListener {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        tilEmail = view.findViewById(R.id.til_email);
+        tilPassword = view.findViewById(R.id.til_password);
         etEmail = view.findViewById(R.id.et_email);
+        etPassword = view.findViewById(R.id.et_password);
+        btnLogin = view.findViewById(R.id.btn_login);
+        tvRegister = view.findViewById(R.id.tv_register);
 
+        btnLogin.setOnClickListener(this);
+        tvRegister.setOnClickListener(this);
 
     }
 
     @Override
+
     public void onClick(View v) {
         switch (v.getId()){
             case R.id.btn_login:
@@ -86,6 +76,7 @@ public class LoginFragment extends Fragment implements View.OnClickListener {
 //                getActivity().getWindow().clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
 //                getActivity().getWindow().clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
 
+                tvRegister.setTextColor(getResources().getColor(R.color.colorCasey));
                 Navigation.findNavController(v).navigate(R.id.action_login_to_register_fragment);
 
                 break;
